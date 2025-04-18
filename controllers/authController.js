55a7b2b7070145742a128474e34036f8eff19f8c
Bishnu336 <07230003.sherubtse@rub.edu.bt>
2025-04-18 22:17:27 +0600

    Practical 5

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,6 +21,7 @@ exports.registerUser = async (req, res) => {
 // Login
 exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
+    const adminEmail = process.env.ADMIN_EMAIL;
 
 
     try {
@@ -39,7 +40,14 @@ exports.loginUser = async (req, res) => {
 
 
         req.session.user = { id: user.id, name: user.name, email: user.email };
-        // res.json({ message: 'Login successful', user: req.session.user });
+
+
+        // Redirect based on role
+        if (user.email === adminEmail) {
+            return res.redirect('/admin/dashboard');
+        }
+
+
         res.redirect('/home');
 
 
